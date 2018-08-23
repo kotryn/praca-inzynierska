962084b8ccae435eeb1e0ef70cc70825893b846a
Katarzyna Romaszewska <kotryn1415@gmail.com>
2018-08-24 01:37:25 +0200

    testy - loguje sie do nowego zadania

--- src/test/java/com/example/kotryn/KotrynApplicationTests.java
+++ src/test/java/com/example/kotryn/KotrynApplicationTests.java
@@ -1,8 +1,12 @@
 package com.example.kotryn;
 
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -15,6 +19,18 @@ public class KotrynApplicationTests {
 		System.setProperty("webdriver.chrome.driver", "/usr/lib/chromium-browser/chromedriver");
 		WebDriver driver=new ChromeDriver();
 		driver.get("http://localhost:8080");
+
+		//driver.findElement(By.name("Begin a new job")).click();
+		driver.findElement(By.xpath("//button[contains(text(),'Begin a new job')]")).click();
+
+		By click = By.xpath("//button[contains(text(),'Connect')]");
+
+		WebDriverWait wait = new WebDriverWait(driver, 10);
+
+		WebElement element = wait.until(ExpectedConditions.presenceOfElementLocated(click));
+
+		//driver.findElement(By.xpath("//button[contains(text(),'Connect')]")).click();
+		element.click();
 	}
 
 }
